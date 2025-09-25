e559e41567ae10b4035cd42360ee9dca9e47ebb6
ravihansa <ravihansad@gmail.com>
2025-09-25 17:03:21 +0530

    Create db connection related changes

--- nodejs-backend/src/configs/database.js
+++ nodejs-backend/src/configs/database.js
@@ -1,7 +1,21 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient({
-    log: ['query', 'info', 'warn', 'error'],
-});
+let prisma;
+if (!globalThis.prisma) {
+    globalThis.prisma = new PrismaClient({ log: ['warn', 'error'] });
+}
+prisma = globalThis.prisma;
+
+const shutdown = async (signal) => {
+    console.info('Shutting down', signal);
+    try {
+        await prisma.$disconnect();
+        console.info('Database connection closed');
+    } catch (e) {
+        console.error(e.message);
+    }
+};
+
+['SIGINT', 'SIGTERM', 'SIGQUIT'].forEach(sig => process.on(sig, () => shutdown(sig)));
 
 export default prisma;
